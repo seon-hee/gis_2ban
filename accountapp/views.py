a9df159f7bea43f8ed00192165a28714bdd39d06
seon-hee <loooooky1234@gmail.com>
2021-07-12 23:12:53 +0900

    7/12 2ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse
+from django.db.models import QuerySet
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 # Create your views here.
 from accountapp.models import HelloWorld
+
 
 def hello_world(request):
     if request.method == "POST":
 
-        temp = request.POST.get('input')
+        temp = request.POST.get('hello_world_input')
 
         new_data = HelloWorld()
         new_data.text = temp
         new_data.save()
 
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
