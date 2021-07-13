91fa90e5f96ca18aed1cb3116996c65ca6a19495
seon-hee <loooooky1234@gmail.com>
2021-07-13 13:57:26 +0900

    7/12 2ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,28 +1,29 @@
-from django.db.models import QuerySet
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse,reverse_lazy
+
+# Create your views here.
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
-
 def hello_world(request):
     if request.method == "POST":
 
-        temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('input')
 
         new_data = HelloWorld()
         new_data.text = temp
         new_data.save()
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+    else:
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'data_list': data_list})
 
 class AccountCreateView(CreateView):
     model = User
